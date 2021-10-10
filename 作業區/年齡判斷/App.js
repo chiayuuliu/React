e3c841e08248a8e8a292bd0c80b694d6f348d17c
Doris Liu <chiayuu.liu@gmail.com>
2021-10-10 15:30:21 +0800

    加入防呆功能,改用改用click觸發判斷

--- 作業區/年齡判斷/App.js
+++ 作業區/年齡判斷/App.js
@@ -4,6 +4,7 @@ import './App.css'
 const App = () => {
 
   const [alerttext, setAlerttext] = useState('')
+  const [yearsold, setYearsold] = useState('')
   const Now = new Date()
   return (
     <>
@@ -11,17 +12,24 @@ const App = () => {
       <h5>請輸入生日</h5>    
       <input type="date"
         onChange={(e)=>{
+          setAlerttext('')
           const birth = new Date(e.target.value)
           const years = (Now-birth)/(24*60*60000*365)
-          console.log(years)
-          if(years>18){
-            // alert('年齡超過18歲')
+          setYearsold(years)
+        }}
+      />
+      <button
+      onClick={()=>{
+        console.log(yearsold)
+        if(!yearsold){
+          alert('請輸入生日')
+          return
+        }if(yearsold>18){
             setAlerttext('年齡超過18歲')
           }else{
             setAlerttext('年齡小於18歲')
           }
-        }}
-      />
+        }}>確認</button>
       {alerttext!=='' &&(<div className="text" >{alerttext}</div> )}
        
     </div>
